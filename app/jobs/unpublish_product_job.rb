704984fb38d27f459c0abdc6f32fc7306ef358f5
thomas rondio <sativva@free.fr>
2019-02-06 12:52:13 +0100

    fix limit3

--- app/jobs/unpublish_product_job.rb
+++ app/jobs/unpublish_product_job.rb
@@ -6,7 +6,7 @@ class UnpublishProductJob < ApplicationJob
     p "___________UnpublishProduct___________"
     # authenticate api
     session_api(params.first[:shop_domain])
-    if  ShopifyAPI::Product.find(:all, params: {limit: 250}).select{|p| p.id.to_s == params.first[:webhook][:id]}.size > 0
+    if ShopifyAPI::Product.find(:all, params: {limit: 250}).select{|p| p.id == params.first[:webhook][:id]}.size > 0
       # unpublish product
       @shopify_product = ShopifyAPI::Product.find(params.first[:webhook][:id])
       @shopify_product.published_at = nil
